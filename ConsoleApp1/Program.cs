9a51123b69c49a3db9006a4ae550b0d51e7f7601
Smurado <62097788+Smurado@users.noreply.github.com>
2022-01-17 14:21:10 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> peter = new List<int>();
+            
+
+            Console.WriteLine(a);
+        }
+    }
+}
